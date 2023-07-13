33c573f5f9e65ffb41030123deee4482a542367d
Mauricio Cardoso <mauricioerick17@gmail.com>
2023-07-13 00:26:55 -0400

    feat: Adding exception handling in AuthController

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Logger;
 use App\Services\AuthService;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -14,19 +16,33 @@ class AuthController extends Controller
 
     public function login(Request $request): JsonResponse
     {
-        $userAuth = $this->authService->login($request->all());
+        try {
+            $userAuth = $this->authService->login($request->all());
 
-        if (!$userAuth) {
-            return response()->json(['message' => 'The Provided credentials are not correct'], 401);
+            if (!$userAuth) {
+                return response()->json(['message' => 'The Provided credentials are not correct'], 401);
+            }
+
+            return response()->json($userAuth);
+        } catch (Exception $e) {
+            $error = 'Unable to log in. Please try again.';
+            Logger::log($e, $error);
+
+            return response()->json(['errors' => $error], 500);
         }
-
-        return response()->json($userAuth);
     }
 
     public function logout(): JsonResponse
     {
-        $this->authService->logout();
+        try {
+            $this->authService->logout();
 
-        return response()->json(['message' => 'Logout successful.']);
+            return response()->json(['message' => 'Logout successful.']);
+        } catch (Exception $e) {
+            $error = 'Unable to log out. Please try again.';
+            Logger::log($e, $error);
+
+            return response()->json(['errors' => $error], 500);
+        }
     }
 }
